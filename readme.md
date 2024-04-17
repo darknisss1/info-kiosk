deb897cd33fb66cd9cfba782d3c9ae37c9a99186
Egor Lynnik <lynnik@moedelo.org>
2024-04-17 16:37:29 +0500

    add init migration

---
+++ readme.md
@@ -0,0 +1,5 @@
+﻿# Запуск миграций
+
+dotnet fm migrate -p postgres -c "Host=localhost; Port=5432; Timeout=600; User ID=postgres; Password=123456; Database=info_
+kiosk" -a .\InfoKiosk.Migrations.dll
+
---
+++ src/InfoKiosk.Migrations/Mg20160219141436_Init.cs
@@ -0,0 +1,15 @@
+﻿using FluentMigrator;
+
+namespace InfoKiosk.Migrations;
+
+[Migration(20160219141436)]
+public class Mg20160219141436Init : Migration
+{
+    public override void Up()
+    {
+    }
+
+    public override void Down()
+    {
+    }
+}
